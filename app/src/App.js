723b1bdc1c2de9feab9cb8231c0b03f04616e985
alobogiron <andregiron.20221@poli.ufrj.br>
2024-05-18 17:29:19 -0300

    new branch

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Form from './Form.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const[sala, setDados] = useState({sala: ''});
+	
+	const handleFormSubmit = (formData) => {
+		setDados(formData);
+	};
+
+	return (
+		<div>
+			<Form onSubmit={handleFormSubmit}/>
+		</div>
+	);
+};
 
 export default App;
