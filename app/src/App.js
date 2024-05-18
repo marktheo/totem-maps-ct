965728c4f48b583d49c5d9617fad37f45edc980e
marktheo <marcosvinytheo@gmail.com>
2024-05-18 17:49:42 -0300

    [Fix]: fix merge problems

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
-import Footer from './Footer';
-
-function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Footer />
-        </div>
-    );
-}
-=======
-import React, {useState} from 'react';
 import Form from './Form.js';
+import Footer from './Footer';
 
 function App() {
 	const[sala, setDados] = useState({sala: ''});
@@ -23,11 +12,12 @@ function App() {
 	};
 
 	return (
-		<div>
+		<div className="App">
+			<Header />
 			<Form onSubmit={handleFormSubmit}/>
+            <Footer />
 		</div>
 	);
 };
->>>>>>> 5e45e67b84822d9df4155a2e4e8e834988e7aa00
 
 export default App;
